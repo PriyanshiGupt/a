69f61b4ea64e5bb9b80f64ce7203b12f8a258880
Priyanshi <priyanshi1072.cse18@chitkara.edu.in>
2020-07-11 15:34:41 +0530

    first commit

---
+++ healthmanagementsystem.py
@@ -0,0 +1,36 @@
+def logdata():
+    reg=input("Are you already registered? y for Yes and n for No")
+    name=input("Please Enter your first name")
+    
+    if reg=='n':
+        with open(name.strip()+'-ex.txt','w') as f:
+            pass
+        with open(name.strip()+'-diet.txt','w') as f:
+            pass
+        
+    work=int(input('Enter 1 for diet and 2 for exercise'))
+    
+    filename=f"{name}{'-ex.txt' if work==2 else '-diet.txt'}"
+    with open(filename,'a') as f:
+        f.write(gettime()+input())
+            
+def retrievedata():
+    name=input("Please Enter your first name")
+    work=int(input('Enter 1 for diet and 2 for exercise'))
+    filename=f"{name}{'-ex.txt' if work==2 else '-diet.txt'}"
+    with open(filename,'r') as f:
+        print(f.read())
+    print("data successfully printed")
+def gettime():
+    import datetime
+    return str(datetime.datetime.now())
+
+n=int(input("Enter 1 to log data and 2 to retrieve"))
+if n==1:
+    logdata()
+elif n==2:
+    retrievedata()
+else:
+    print('invalid input')
+   
+    
