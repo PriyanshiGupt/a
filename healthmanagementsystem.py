a2832cb59ca86abe66c42733e9c574a454879cda
Priyanshi <priyanshi1072.cse18@chitkara.edu.in>
2020-07-11 15:45:22 +0530

    2nd commit

--- healthmanagementsystem.py
+++ healthmanagementsystem.py
@@ -1,6 +1,7 @@
+#health management system
 def logdata():
-    reg=input("Are you already registered? y for Yes and n for No")
-    name=input("Please Enter your first name")
+    reg=input("Are you already registered? y for Yes and n for No : ")
+    name=input("Please Enter your first name : ")
     
     if reg=='n':
         with open(name.strip()+'-ex.txt','w') as f:
@@ -8,15 +9,15 @@ def logdata():
         with open(name.strip()+'-diet.txt','w') as f:
             pass
         
-    work=int(input('Enter 1 for diet and 2 for exercise'))
+    work=int(input('Enter 1 for diet and 2 for exercise : '))
     
     filename=f"{name}{'-ex.txt' if work==2 else '-diet.txt'}"
     with open(filename,'a') as f:
-        f.write(gettime()+input())
-            
+        f.write(gettime()+' '+input()+'\n')
+    print("data added successfully")
 def retrievedata():
-    name=input("Please Enter your first name")
-    work=int(input('Enter 1 for diet and 2 for exercise'))
+    name=input("Please Enter your first name : ")
+    work=int(input('Enter 1 for diet and 2 for exercise : '))
     filename=f"{name}{'-ex.txt' if work==2 else '-diet.txt'}"
     with open(filename,'r') as f:
         print(f.read())
@@ -25,7 +26,7 @@ def gettime():
     import datetime
     return str(datetime.datetime.now())
 
-n=int(input("Enter 1 to log data and 2 to retrieve"))
+n=int(input("Enter 1 to log data and 2 to retrieve : "))
 if n==1:
     logdata()
 elif n==2:
